d06ec0e34e1ce7d944d67f63291f5d513118775d
Ricardo <ricardo.ortega97@outlook.com>
2024-01-26 23:28:33 -0500

    "ContactDBHelper created/updated"

--- app/src/main/java/com/example/mycontactlist/ContactDBHelper.java
+++ app/src/main/java/com/example/mycontactlist/ContactDBHelper.java
@@ -1,4 +1,35 @@
 package com.example.mycontactlist;
 
-public class ContactDBHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class ContactDBHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "mycontacts.db";
+    private static final int DATABASE_VERSION = 1;
+    //database creation sql statement
+    private static final String CREATE_TABLE_CONTACT =
+            "create table contact (_id integer primary key autoincrement, "
+            + "contactname text not null, streetaddress text, "
+            + "city text, state text, zipcode text, "
+            + "phonenumber text, cellnumber text,"
+            + "email text, birthday text );";
+    //constructor method calls super constructor method & creates a new instance of ContactDBHelper
+    public ContactDBHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    @Override //this method calls first time the database is opened
+    public void onCreate(SQLiteDatabase db){
+        db.execSQL(CREATE_TABLE_CONTACT);
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+        Log.w(ContactDBHelper.class.getName(),
+        "Upgrading database from version " + oldVersion + " to "
+                + newVersion + ", which will destroy all old data");
+        db.execSQL("DROP TABLE IF EXISTS contact");
+        onCreate(db);
+    }
 }
