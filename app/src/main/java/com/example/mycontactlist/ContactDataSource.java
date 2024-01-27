2686551f003af2b09bd890ce7dc33f72b1ccff12
Ricardo <ricardo.ortega97@outlook.com>
2024-01-27 00:05:24 -0500

    "ContactDataSrc updated "

--- app/src/main/java/com/example/mycontactlist/ContactDataSource.java
+++ app/src/main/java/com/example/mycontactlist/ContactDataSource.java
@@ -1,5 +1,6 @@
 package com.example.mycontactlist;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
@@ -18,4 +19,49 @@ public class ContactDataSource {
     public void close() {
         database.close();
     }
+    public boolean insertContact(Contact c) {
+        boolean didSucceed = false;
+        try {
+            ContentValues initialValues = new ContentValues();
+
+            initialValues.put("contactname", c.getContactName());
+            initialValues.put("streeaddress", c.getStreetAddress());
+            initialValues.put("city", c.getCity());
+            initialValues.put("state", c.getState());
+            initialValues.put("zipcode", c.getZipcode());
+            initialValues.put("phonenumber", c.getPhoneNumber());
+            initialValues.put("cellnumber", c.getCellNumber());
+            initialValues.put("email", c.geteMail());
+            initialValues.put("birthday", String.valueOf(c.getBirthday().getTimeInMillis()));
+
+            didSucceed = database.insert("contact", null, initialValues) > 0;
+        }catch (Exception e) {
+            //Do nothing = will return false if there is an exception
+        }
+        return didSucceed;
+    }
+    public boolean updateContact(Contact c) {
+        boolean didSucceed = false;
+        try{
+            Long rowID = (long) c.getContactID();
+            ContentValues updateValues = new ContentValues();
+
+            updateValues.put("contactname", c.getContactName());
+            updateValues.put("streeaddress", c.getStreetAddress());
+            updateValues.put("city", c.getCity());
+            updateValues.put("state", c.getState());
+            updateValues.put("zipcode", c.getZipcode());
+            updateValues.put("phonenumber", c.getPhoneNumber());
+            updateValues.put("cellnumber", c.getCellNumber());
+            updateValues.put("email", c.geteMail());
+            updateValues.put("birthday",
+                    String.valueOf(c.getBirthday().getTimeInMillis()));
+
+            didSucceed = database.update("contact", updateValues, "_id" + rowID, null) > 0;
+        }catch (Exception e) {
+            //Do nothing -will return false if there is an expection
+        }
+        return didSucceed;
+    }
+
 }
