679f16157196d57568dbd36efa75210c910744f1
Ricardo <ricardo.ortega97@outlook.com>
2024-02-10 02:55:48 -0500

    "initLocationButton Added "

--- app/src/main/java/com/example/mycontactlist/ContactMapActivity.java
+++ app/src/main/java/com/example/mycontactlist/ContactMapActivity.java
@@ -1,21 +1,25 @@
 package com.example.mycontactlist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
-import android.location.Address;
-import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import java.io.IOException;
-import java.util.List;
+import android.widget.Toast;
+
 
 public class ContactMapActivity extends AppCompatActivity {
-
+    //variable declarations
+    LocationManager locationManager;
+    LocationManager gpsListenser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,31 +65,43 @@ public class ContactMapActivity extends AppCompatActivity {
         locationButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText editAddress = (EditText) findViewById(R.id.addressInputMap);
-                EditText editCity = (EditText) findViewById(R.id.editCity);
-                EditText editSate = (EditText) findViewById(R.id.editState);
-                EditText editZipcode = (EditText) findViewById(R.id.editZipcode);
 
-                String address = editAddress.getText().toString() + ","  +
-                        editCity.getText().toString() + ","  +
-                        editSate.getText().toString() + ","  +
-                        editZipcode.getText().toString();
-                //create a list object that holds the address to declare
-                List<Address> addresses = null;
-                Geocoder geo = new Geocoder(ContactMapActivity.this);
-                try { //try catch method to protect the app from errors
-                    addresses = geo.getFromLocationName(address, 1);
+                try {
+                    locationManager = (LocationManager)getBaseContext().
+                            getSystemService(Context.LOCATION_SERVICE);
+                    gpsListenser = new LocationListener() {
+                        @Override
+                        public void onLocationChanged(@NonNull Location location) {
+                            TextView txtLatitude = (TextView) findViewById(R.id.textLatitude);
+                            TextView txtLongitude = (TextView) findViewById(R.id.textLongitude);
+                            TextView txtAccuracy = (TextView) findViewById(R.id.textAccuracy);
+                            txtLatitude.setText(String.valueOf(location.getLatitude()));
+                            txtLongitude.setText(String.valueOf(location.getLongitude()));
+                            txtAccuracy.setText(String.valueOf(location.getAccuracy()));
+                        }
+                        public void onStatusChanged(String provider, int status, Bundle extras) {}
+                        public void onProviderEnabled(String provider) {}
+                        public void onProviderDisabled(String provider) {}
+                    };
+                    locationManager.requestLocationUpdates(
+                            LocationManager.GPS_PROVIDER,0, 0,gpsListenser);
                 }
-                catch (IOException e) {
-                    e.printStackTrace();
+                catch (Exception e) {
+                    Toast.makeText(getBaseContext(), "Error, Location not available",
+                            Toast.LENGTH_LONG).show();
                 }
-                TextView txtLatitude = findViewById(R.id.textLatitude);
-                TextView txtLongitude = findViewById(R.id.textLongitude);
-
-                txtLatitude.setText(String.valueOf(addresses.get(0).getLatitude()));
-                txtLongitude.setText(String.valueOf(addresses.get(0).getLongitude()));
             }
         });
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        try {
+            locationManager.removeUpdates(gpsListenser);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
